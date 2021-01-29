abae6da78d97e6fc3b39894a19662d262214ea51
Federico Brunelli <federico.brunelli4@studio.unibo.it>
2021-01-29 16:59:23 +0100

    Nesting2 Completed

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,44 @@
+package it.unibo.oop.lab.nesting2;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+
+	private final List<T> listAcceptable;
+	
+	public OneListAcceptable(final List<T> list) {
+		this.listAcceptable = list;
+	}
+	@Override
+	public Acceptor<T> acceptor() {
+		final Iterator<T> iterator = listAcceptable.iterator();
+		return new Acceptor<T>() {
+
+			@Override
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				try {
+					if (newElement.equals(iterator.next())) {
+						return;
+					}
+				} catch(Exception e) {
+					System.out.println("No more element");
+				}
+				throw new Acceptor.ElementNotAcceptedException(newElement);
+			}
+
+			@Override
+			public void end() throws EndNotAcceptedException {
+				try {
+					if (!iterator.hasNext()) {
+						return;
+					}
+				}catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				throw new Acceptor.EndNotAcceptedException();
+			}			
+		};
+	}
+
+}
